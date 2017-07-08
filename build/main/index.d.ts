7cf5dd7cb6344b064e9a875cfa6ec39f82ae08d1
RyanRio <thermitchellman@gmail.com>
2017-07-08 08:45:07 -0400

    patch

--- build/main/index.d.ts
+++ build/main/index.d.ts
@@ -1,4 +1,4 @@
-export { matrix } from './lib/core/matrix';
+export { matrix, Matrix } from './lib/core/matrix';
 export { ones } from './lib/core/ones';
 export { zeros } from './lib/core/zeros';
 export { identityMatrix } from './lib/core/identityMatrix';
